78f1b90ed94d1576b14c4e7e6c6c1209bc8186e8
Joseph Karim <joekarim87@gmail.com>
2025-05-02 17:15:15 -0400

    Add Coming Soon badge to Expand Into New Regions card in SeparateWorkLinePage

--- src/components/SeparateWorkLinePage.tsx
+++ src/components/SeparateWorkLinePage.tsx
@@ -907,9 +907,9 @@ export default function SeparateWorkLinePage() {
                   </div>
                 </div>
                 <div className="flex-shrink-0 ml-4">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                    <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
-                  </svg>
+                  <div className="px-2 py-1 bg-yellow-100 text-yellow-800 text-xs rounded-full">
+                    Coming Soon
+                  </div>
                 </div>
               </div>
             </motion.div>
